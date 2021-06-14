c029180b04cc7286a281e084687ee6e86f34c2ed
Kim Ford <kforddorsey@cnm.edu>
2021-06-14 16:50:13 -0600

    Started JavaDoc comments.

--- src/main/java/edu/cnm/deepdive/Shuffler.java
+++ src/main/java/edu/cnm/deepdive/Shuffler.java
@@ -2,10 +2,18 @@ package edu.cnm.deepdive;
 
 import java.util.Random;
 
+/**
+ * Implements <a href="https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle">Fisher-Yates</a>  shuffle for all primitive data types, as well as {@link Object}. Shuffling is  done in place&mdash; that is, the methods don't return compies of the original arrays, but instead modify the contents directly.
+ */
 public class Shuffler {
+
 
   private final Random rng;
 
+  /**
+   * Iinitializes this instance, using the provided source of randomness.
+   * @param rng Source of randomness.
+   * */
 
   public Shuffler(Random rng) {
     this.rng = rng;
@@ -21,5 +29,29 @@ public class Shuffler {
     }
 
   }
+
+  public void shuffle(byte[] data) {
+    for (int targetIndex = data.length -1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      byte temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data [sourceIndex] = temp;
+
+    }
+
+  }
+
+  public void shuffle(Object[] data) {
+    for (int targetIndex = data.length -1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      Object temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data [sourceIndex] = temp;
+
+    }
+
+  }
+
+
 
 }
